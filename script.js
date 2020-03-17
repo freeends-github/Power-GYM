38e087462c8ac992f75f4de71830d0f21a459d1b
Ragip Gjinovci <ragip.gjinovci@probit-kos.com>
2020-03-18 00:08:15 +0100

    power

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// slider classes
+
+
+// let ourPricing = document.querySelector("#header");
+// var sliderImage = ["url(./imgs/headerBg2x.png)", "url(./imgs/hamburger-and-fries-beside-mug-with-beer-3356408.jpg)", "url(./imgs/corona-background.jpg)"];
+// const filterArrowLeft = document.querySelector("#filter_arrow-left");
+// const filterArrowRight = document.querySelector("#filter_arrow-right");
+// const classesSlider = document.querySelector("#filter-slider");
+// const menFilter = document.querySelector("#showmen");
+// const womenFilter = document.querySelector("#showwomen");
+
+// let current = 0;
+// let item;
+// function removeChild(x){
+//   classesSlider.removeChild(classesSlider.childNodes[x])
+// }
+// function removeLastChild(){
+//   classesSlider.removeChild(classesSlider.lastChild)
+// }
+// if(current === 0){
+//   filterArrowLeft.addEventListener('click', function(){
+//     classesSlider.style.transform = 'translateX(-10px)'
+//     classesSlider.style.transition = 'all ease-in-out 0.25s'
+//   })
+// }
+
+// filterArrowRight.addEventListener('click', function(){
+//   removeChild(current);
+// })
+// filterArrowLeft.addEventListener('click', function(){
+  
+// })
 
 //Modal (get started buttons)
 var getStartedButtons = document.querySelectorAll('.choose-plan');
@@ -23,7 +55,7 @@ noButton.addEventListener('click', closeModal);
   'use strict';
 
   var $filters = $('.filter-bar [data-filter]'),
-    $boxes = $('.classes [data-category]');
+      $boxes = $('.classes [data-category]');
   
 
   $filters.on('click', function(e) {
@@ -34,24 +66,31 @@ noButton.addEventListener('click', closeModal);
     $this.addClass('active');
 
     var $filterColor = $this.attr('data-filter');
-
-    if ($filterColor == 'all') {
+    var $category = $this.attr('data-category');
+    
       $boxes.removeClass('is-animated')
         .fadeOut().finish().promise().done(function() {
           $boxes.each(function(i) {
             $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
           });
         });
-    } else if($filterColor == 'men'){
-       $boxes.removeClass('is-animated')
-         .fadeOut().finish().promise().done(function() {
-           $boxes.filter('[data-category = "' + $filterColor + '"]').each(function(i) {
-             $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
-           });
-        });
+    if( $category == 'man'){
+        $boxes.removeClass('is-animated')
+          .fadeOut().finish().promise().done(function() {
+            $boxes.filter('[data-category = "' + $category + '"]').each(function(i) {
+            $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+          });
+       });
     }
+    // else if($category == 'woman'){
+    //   $boxes.removeClass('is-animated')
+    //       .fadeOut().finish().promise().done(function() {
+    //         $boxes.filter('[data-category = "' + $category + '"]').each(function(i) {
+    //         $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+    //       });
+    // })
   });
-  //coming back
+//comming back
 
 })(jQuery);
 
