f9918e4da2fb65d0a6c5e5389c92b1acfb711902
Fatbardh <fatbardhemini5@gmail.com>
2020-03-24 22:26:35 -0700

    Final version with responsive layout

--- app.js
+++ app.js
@@ -11,3 +11,32 @@ input.addEventListener("click",function(){
 		}
 	})
 });
+
+const items = document.querySelectorAll('.classes_items');
+
+items.forEach(function(aa){
+	const plus = aa.children[2];
+	const info = aa.children[1];
+	plus.addEventListener("click",function(){
+		info.classList.toggle("appair");
+		plus.classList.toggle("rotate");
+	});
+});
+
+const hamburger = document.querySelector('.list_container i')
+const pop = document.querySelector('.list_pop')
+const x = document.querySelector('.list_pop i');
+const a = document.querySelector('.list_pop ul');
+
+hamburger.addEventListener("click", function(){
+	pop.style.height = '100vh';
+	pop.style.transition = ' height 800ms ease-out';
+	a.style.display='block';
+	x.style.display='block';
+});
+
+x.addEventListener("click", function(){
+	pop.style.height = '0vh';
+	a.style.display='none';
+	x.style.display='none';
+});
